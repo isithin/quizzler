5e1e4f1f197c2bc75d92d842a246c7caf2a48645
Isabel Metzelthin <isabel.metzelthin@strato.de>
2023-08-03 15:38:37 +0200

    server start into app

--- client/src/main/java/oop/quizzler/controller/WelcomeController.java
+++ client/src/main/java/oop/quizzler/controller/WelcomeController.java
@@ -2,7 +2,9 @@ package oop.quizzler.controller;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import oop.quizzler.model.Connection;
 
 /**
@@ -17,15 +19,34 @@ public class WelcomeController {
 	private Button joinGame;
 
 	/**
-	 * Switches to the menu view. Establishes a connection to the local server.
+	 * Switches to the menu view. Starts the local server and establishes a
+	 * connection. The connection is saved in the InitQuizzler class. If the server
+	 * is already running, the menu view is shown. If the server start fails, an
+	 * alert is shown.
+	 * 
 	 * 
 	 * @throws IOException if the view cannot be found
 	 */
 	@FXML
-	private void switchToMenu() throws IOException {
-		Connection connection = new Connection("127.0.0.1", 1099);
-		InitQuizzler.setConnection(connection);
-		InitQuizzler.setRoot("menu");
+	private void switchToMenu() {
+		try {
+			String ip = StartServer.start();
+			if (ip != null && InitQuizzler.getConnection() == null) {
+				Connection connection = new Connection(ip, 1099);
+				InitQuizzler.setConnection(connection);
+				InitQuizzler.setRoot("menu");
+			} else if (ip == null && InitQuizzler.getConnection() != null) {
+				InitQuizzler.setRoot("menu");
+			} else if (ip == null && InitQuizzler.getConnection() == null) {
+				Alert alert = new Alert(Alert.AlertType.NONE, "Server start failed", ButtonType.OK);
+				alert.showAndWait();
+				if (alert.getResult().equals(ButtonType.OK)) {
+					alert.close();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
